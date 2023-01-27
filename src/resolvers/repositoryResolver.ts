cb4048ca84449062996f7c2c795759f5f2aabcc4
thee <theekshana.sandaru@gmail.com>
2023-01-28 00:17:16 +0800

    updated

--- src/resolvers/repositoryResolver.ts
+++ src/resolvers/repositoryResolver.ts
@@ -6,12 +6,12 @@ import { RepositoryResponse } from "../types/repositoryResponse";
 
 @Resolver()
 export class GitHubRepositoris {
-    @Query(_returns => RepositoryResponse, { nullable: false })
-    async gitRepositories(@Arg("limit") limit: number, @Arg("method") method: string) {
-        console.log(limit)
-        const headers = new Headers()
-        const payload = JSON.stringify({
-            query: `{
+  @Query(_returns => RepositoryResponse, { nullable: false })
+  async gitRepositories(@Arg("limit") limit: number, @Arg("method") method: string) {
+    console.log(limit)
+    const headers = new Headers()
+    const payload = JSON.stringify({
+      query: `{
                 viewer {
                     login
                     name
@@ -47,43 +47,43 @@ export class GitHubRepositoris {
                     }
                 }
             }`,
-        });
+    });
 
-        headers.append("Content-Type", "application/graphql")
-        headers.append("Authorization", "Bearer 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")
-        // console.log(typeof payload)
-        const requestOption = {
-            method: "POST",
-            headers,
-            body: payload
-        }
-        const repos: RepositoryResponse = {
-            name: "",
-            login: "",
-            reposiories: [{
-                id: "",
-                description: "",
-                url: "",
-                name: "",
-                primaryLanguage: {
-                    name: ""
-                },
-                updatedAt: Date.now().toString()
-            }]
-        }
-        try {
-            const response = await fetch("https://api.github.com/graphql", requestOption)
-            const json = await response.json()
-            repos.name = json.data.viewer.name
-            repos.login = json.data.viewer.login
-            repos.reposiories = json.data.viewer.repositories.nodes
-            console.log(json.data.viewer.repositories.nodes)
-        } catch (e) {
-            console.log(e)
-        }
-
-        console.log(repos)
-
-        return repos
+    headers.append("Content-Type", "application/graphql")
+    headers.append("Authorization", "Bearer")
+    // console.log(typeof payload)
+    const requestOption = {
+      method: "POST",
+      headers,
+      body: payload
     }
+    const repos: RepositoryResponse = {
+      name: "",
+      login: "",
+      reposiories: [{
+        id: "",
+        description: "",
+        url: "",
+        name: "",
+        primaryLanguage: {
+          name: ""
+        },
+        updatedAt: Date.now().toString()
+      }]
+    }
+    try {
+      const response = await fetch("https://api.github.com/graphql", requestOption)
+      const json = await response.json()
+      repos.name = json.data.viewer.name
+      repos.login = json.data.viewer.login
+      repos.reposiories = json.data.viewer.repositories.nodes
+      console.log(json.data.viewer.repositories.nodes)
+    } catch (e) {
+      console.log(e)
+    }
+
+    console.log(repos)
+
+    return repos
+  }
 }
